58570a14519c3faad3894b3a25047d0be175a303
diegom0n <die.fuentes@duocuc.cl>
2023-12-20 03:23:02 -0300

    Follow Service
    si

---
+++ twitch2/app/(browse)/[username]/_components/actions.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useTransition } from "react";
+
+import { onFollow, onUnfollow } from "@/actions/follow";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+
+interface ActionsProps {
+    isFollowing: boolean;
+    userId: string;
+};
+
+export const Actions = ({
+    isFollowing,
+    userId,
+}: ActionsProps) => {
+    const [isPending, startTransition] = useTransition();
+
+    const handleFollow = () => {
+        startTransition(() => {
+            onFollow(userId)
+                .then((data) => toast.success(`Ahora sigues a ${data.following.username}`))
+                .catch(() => toast.error("Something went wrong"));
+        });
+    };
+
+    const handleUnfollow = () => {
+        startTransition(() => {
+            onUnfollow(userId)
+                .then((data) => toast.success(`Dejaste de seguir a ${data.following.username}`))
+                .catch(() => toast.error("Something went wrong"));
+        });
+    };
+
+    const onClick = () => {
+        if (isFollowing) {
+            handleUnfollow();
+        } else {
+            handleFollow();
+        }
+    }
+
+    return (
+
+        <Button 
+        disabled={isPending} 
+        onClick={onClick} 
+        variant="primary"
+        >
+            {isFollowing ? "Unfollow" : "Follow"}
+        </Button>
+    );
+};
