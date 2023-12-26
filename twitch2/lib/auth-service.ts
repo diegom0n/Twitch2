bc1a64745437b79e7871118b369c92715ee11718
diegom0n <die.fuentes@duocuc.cl>
2023-12-25 23:33:01 -0300

    Chat settings
    creo que igual agregué algo al dashboard

--- twitch2/lib/auth-service.ts
+++ twitch2/lib/auth-service.ts
@@ -6,7 +6,7 @@ export const getSelf = async () => {
     const self = await currentUser();
 
     if (!self || !self.username) {
-        throw new Error("Unathorized");
+        throw new Error("No compare ✋");
     }
 
     const user = await db.user.findUnique({
@@ -15,6 +15,28 @@ export const getSelf = async () => {
 
     if (!user) {
         throw new Error("Not found");
+    }
+
+    return user;
+};
+
+export const getSelfByUsername = async (username: string) => {
+    const self = await currentUser();
+
+    if (!self || !self.username) {
+        throw new Error("No compare ✋");
+    }
+
+    const user = await db.user.findUnique({
+        where: { username }
+    });
+
+    if (!user) {
+        throw new Error("User not found")
+    }
+
+    if (self.username !== user.username) {
+        throw new Error("No compare ✋");
     }
 
     return user;
