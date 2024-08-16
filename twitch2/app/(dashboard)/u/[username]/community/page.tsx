e94a239ac889c986d44df6dd04a35006e06641dc
diegom0n <die.fuentes@duocuc.cl>
2024-08-16 00:16:08 -0400

    Search Page

---
+++ twitch2/app/(dashboard)/u/[username]/community/page.tsx
@@ -0,0 +1,13 @@
+const CommunityPage = () => {
+    return (
+        <div className="p-6">
+            <div className="mb-4">
+                <h1 className="text-2xl font-bold">
+                    Community Settings
+                </h1>
+            </div>
+        </div>
+    );
+}
+
+export default CommunityPage;
